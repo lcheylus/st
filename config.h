491601f603918a6ccab67d005a847b8217eaa799
Laurent Cheylus <foxy@free.fr>
2024-06-24 17:39:31 +0200

    Apply font2 patch
    - This patch allows to add spare font besides default.
    - Patch https://st.suckless.org/patches/font2/st-font2-0.8.5.diff

--- config.h
+++ config.h
@@ -7,6 +7,10 @@
  */
 // static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static char *font = "Cousine Nerd Font:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true"
+};
 
 static int borderpx = 2;
 
