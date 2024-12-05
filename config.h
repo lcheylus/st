e98b3a1074cbaa67bd19445a7f7cb1980f8853b6
Laurent Cheylus <foxy@free.fr>
2024-12-05 15:57:55 +0100

    config.h: use Jet Brains Mono font with Regular style

--- config.h
+++ config.h
@@ -6,13 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-// static char *font = "MartianMono NF Med:pixelsize=11:antialias=true:autohint=true";
-// static char *font = "Cousine Nerd Font:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Cousine Nerd Font:weight=bold:size=12:pixelsize=12:antialias=true:autohint=true";
+// static char *font = "Cousine Nerd Font:weight=bold:size=12:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:weight=100:size=12:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
 
-static int borderpx = 2;
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
