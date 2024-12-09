b1cbce0b9804bb81c1c446b37b4c007e4b417bd8
Laurent Cheylus <foxy@free.fr>
2024-12-09 17:16:21 +0100

    config.h: set chscale value to 1.0 with JetBrains Mono font
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- config.h
+++ config.h
@@ -32,8 +32,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-// static float chscale = 1.0;
-static float chscale = 0.8;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
